34f9fbae1315a16f592875c8a38fe9c9970eeae5
Maciej Kamela <maciej.kamela@pearson.com>
2015-11-18 08:49:57 +0100

    x

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,10 +2,8 @@
 var app = app || {};
 app.Calendar = function ($calendarContainer, monthNames, dayNames) {
     this.$calendarContainer = $calendarContainer;
-    //this.$calendarWrapper = $('<div>').addClass('calendar-wrapper');
     this.$calendarWrapper = $('<section>').addClass('calendar-wrapper');
     this.$calendarContainer.append(this.$calendarWrapper);
-    //this.$calendarWrapper.append(this.$calendarIsotop);
     this.monthName = monthNames;
     this.monthAmount = monthNames.length;
     this.dayNames = dayNames;
@@ -16,10 +14,10 @@ app.Calendar.prototype.createCalendars = function (row, column) {
 
     for (var i = 0; i < this.monthAmount; i++) {
         var li = $('<div>').addClass('row col-xs-12 col-sm-6 col-md-3 calendar-item');
-        for(var j = 0; j < this.daysAmount; j++) {
+        for (var j = 0; j < this.daysAmount; j++) {
             var $table = this.createTable(row, column, this.dayNames[j], this.monthName[i]);
         }
-        //var $table = this.createTable(row, column, this.dayName, this.monthName[i]);
+        //var $table = this.createTable(row, column, this.dayNames, this.monthName[i]);
         li.append($table);
         this.$calendarWrapper.append(li);
     }
@@ -35,18 +33,15 @@ app.Calendar.prototype.createTable = function (numRows, numCols, dayName, monthN
         $calendarHeader = $("<tr>").addClass('pn-calendar-header'),
         $headerMonth = $("<th>").addClass('pn-calendar-header-month').attr('colspan', 7).text(monthName),
         $subHeader = $("<tr>").addClass('pn-calendar-subheader');
-    console.log(dayName, dayName.length, numCols, numRows, monthName);
+    //console.log(dayName, dayName.length, numCols, numRows, monthName);
     var $day = numCols.times(function () {
-        //console.log(dayName, dayName.length);
-        return $("<td/>").addClass('pn-calendar-header-day').text(dayName);
-        //for (var i = 0; i < dayName.length; i++) {
-        //    return $("<td/>").addClass('pn-calendar-header-day').text(dayName[i]);
-        //}
-
-    });
+            return $("<td/>").addClass('pn-calendar-header-day').text(dayName);
+        }
+    );
+    //var $day = $("<td/>").addClass('pn-calendar-header-day').text(dayName);
     var row = function () {
         return $("<tr/>").append(numCols.times(function (c) {
-            return $("<td/>").addClass('pn-calendar-day').text([c].join(""));
+            return $("<td/>").addClass('pn-calendar-day').text([c+1].join(""));
         }));
     };
     $subHeader.append($day);
