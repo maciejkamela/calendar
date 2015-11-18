8518e6aa786229ad6574875c263cc18e09972d6f
camel <camel>
2015-11-18 20:36:07 +0100

    Adding days

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -36,15 +36,6 @@ app.Calendar.prototype.appendDaysHeaders = function ($container) {
     }
 };
 
-app.Calendar.prototype.appendDaysHeaders = function ($container) {
-    var $td;
-    for (var i = 0; i < this.DAYS; i++) {
-        $td = $("<td/>").addClass('pn-calendar-header-day');
-        $td.text(this.dayNames[i]);
-        $container.append($td);
-    }
-};
-
 app.Calendar.prototype.setDates = function () {
 
     /*
@@ -62,27 +53,41 @@ app.Calendar.prototype.getSelectedDates = function () {
 
 
 app.Calendar.prototype.appendDays = function ($container, monthIndex) {
+
     var daysInMonth = this.getDaysInMonth(monthIndex + 1, this.currentYear);
-    console.log(daysInMonth);
+    console.log((monthIndex + 1) + 'miesiac zaczyna sie od ', this.firstDayInMonth(monthIndex));
+
     for (var i = 0; i < this.ROWS; i++) {
         var $row = $("<tr/>");
         //TODO dodaj dni, musisz znalezc index dnia dla 1 dnia kazdego miesiaca, w parametrze ta metoda przyjmuje monthIndex 0 - styczen, 1- luty itd..
+        this.createDays($row, monthIndex);
+        //for (var j = 0; j < this.DAYS; j++) {
+        //    var $cell = $('<td/>').addClass('pn-calendar-day');
+        //    $row.append($cell.text(j));
+        //}
         $container.append($row);
     }
-    /*
-     var $td;
-     for (var i = 0; i < this.DAYS; i++) {
-     $td =  $("<td/>").addClass('pn-calendar-header-day');
-     $td.text(this.dayNames[i]);
-     $container.append($td);
-     }
-     */
+};
+app.Calendar.prototype.firstDayInMonth = function (monthIndex) {
+    var newDate = new Date(this.currentYear, monthIndex, 1);
+    return newDate.getDay();
 };
 
+app.Calendar.prototype.createDays = function ($container, monthIndex) {
+    var daysInMonth = this.getDaysInMonth(monthIndex + 1, this.currentYear);
+    for (var i = 0; i < this.DAYS; i++) {
+        var $cell = $('<td/>').addClass('pn-calendar-day ');
+        for (var k = 0; k < daysInMonth; k++) {
+            $cell.text(k + 1);
+        }
+        $container.append($cell);
+    }
+
+};
 app.Calendar.prototype.createTable = function (monthIndex) {
     var $tableHead = $("<thead>"),
         $calendarHeader = $("<tr>").addClass('pn-calendar-header'),
-        $headerMonth = $("<th>").addClass('pn-calendar-header-month').attr('colspan', 7).text(this.monthName[monthIndex]),
+        $headerMonth = $("<th>").addClass('pn-calendar-header-month').attr('colspan', 7).text(this.monthName[monthIndex] + ' ' + this.currentYear),
         $subHeader = $("<tr>").addClass('pn-calendar-subheader'),
         $table = $("<table/>").addClass('pn-calendar');
 
